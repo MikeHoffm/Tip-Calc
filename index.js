5e5e9244ed5d9bdb158e623972cb4d6aadce81e1
Mike Hoffm <mhoffm24@gmail.com>
2023-04-03 13:58:30 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const formSubmit = document.querySelectorAll('.submit');
+formSubmit.style.background = 'blue';
+console.log('hELLO);');
+
+formSubmit.addEventListener('click', () => {
+  event.preventDefault();
+});
