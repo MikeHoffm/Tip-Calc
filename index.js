8ca8a6814d7824f6179899f4ded695fba1367870
Mike Hoffm <mhoffm24@gmail.com>
2023-04-03 14:25:58 -0400

    Basic JS functionality added

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const formSubmit = document.querySelectorAll('.submit');
-formSubmit.style.background = 'blue';
-console.log('hELLO);');
+const formSubmit = document.querySelector('.submit');
 
 formSubmit.addEventListener('click', () => {
   event.preventDefault();
+  calcTip(billTotal.value, tipPercent.value);
 });
+
+const billTotal = document.getElementById('bill');
+const tipPercent = document.getElementById('percentage');
+
+function calcTip(bill, tip) {
+// billTotal * (1 + tipPercentage) as a decimal
+// ex for 20%: billTotal * (1 + .20) will give the Total of bill + tip
+  percentToDecimal = parseFloat(tip) / 100;
+  const billTipIncluded = bill * (1 + percentToDecimal);
+  console.log(billTipIncluded);
+  return billTipIncluded;
+  // take value from above, subtract amount of bill to get the tip amount
+}
+
+// when submit is pressed, call calcTip(billTotal, tipPercentage)
+// update DOM to show the Total with tip included, and the tip
